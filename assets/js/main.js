2b977199bcf70f3b59e2568c02d5659a5ab08012
MaheshBhushan <mkoduri73@gmail.com>
2025-05-16 01:04:45 +0200

    final education details updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -249,14 +249,7 @@
    */
   const ageElement = document.getElementById('age');
   if (ageElement) {
-    const birthDate = new Date('2003-08-27');
-    const today = new Date();
-    let age = today.getFullYear() - birthDate.getFullYear();
-    const m = today.getMonth() - birthDate.getMonth();
-    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
-      age--;
-    }
-    ageElement.textContent = age;
+    ageElement.textContent = "22";
   }
 
   /**
